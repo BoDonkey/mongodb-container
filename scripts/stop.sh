b3388e595869c2b907f8a0e1b204094f846fc748
Robert Means <robert@robertmeans.net>
2025-02-01 08:27:52 -0500

    Update stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 if command -v docker &> /dev/null; then
     docker compose down
 elif command -v podman &> /dev/null; then
-    podman-compose down
+    podman compose down
 else
     echo "Error: Neither Docker nor Podman found"
     exit 1
